28d7c2dfb2a51edaa1b4388c381a47e616b00008
antonio moreno <amorenomtz@gmail.com>
2022-10-17 08:52:40 +0200

    ending the first stage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,13 @@
-# Rails.application.config.precompile += %w( admin.js admin.css )
-Rails.application.config.paths << Rails.root.join("node_modules")
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your
-Rails.application.config.version = "1.0"
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-# Rails.application.config.paths << Emoji.images_path
+# Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
